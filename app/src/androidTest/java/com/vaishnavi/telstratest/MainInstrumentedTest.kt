329793129fa9583136634c62f9399ed1c1186ad7
vaishnavi-vijendrachar <vaishnavi.vijendrachar@gmail.com>
2020-03-25 18:42:14 +1100

    code cleanup

--- app/src/androidTest/java/com/vaishnavi/telstratest/MainInstrumentedTest.kt
+++ app/src/androidTest/java/com/vaishnavi/telstratest/MainInstrumentedTest.kt
@@ -7,10 +7,10 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.uiautomator.UiDevice
+import com.vaishnavi.telstratest.ui.main.MainActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
